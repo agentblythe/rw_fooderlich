b703127fee9cfa8184bc3e95dd80035b7e5919c9
Steve Blythe <blythesr@gmail.com>
2022-03-04 21:48:06 +0000

    Finished Card2

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'author_card.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -25,11 +26,36 @@ class Card2 extends StatelessWidget {
           ),
         ),
         child: Column(
-          children: const [
-            AuthorCard(
+          children: <Widget>[
+            const AuthorCard(
               title: 'Smoothie Connoisseur',
               authorName: 'Mike Katz',
               imageProvider: AssetImage('assets/author_katz.jpeg'),
+            ),
+            Expanded(
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
