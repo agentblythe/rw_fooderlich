cc3888d8502650ff86988b10d57f3e0ab455ce06
Steve Blythe <blythesr@gmail.com>
2022-03-06 20:41:15 +0000

    Add asset dirs to yaml, added basic explore screen

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatelessWidget {
+  ExploreScreen({Key? key}) : super(key: key);
+
+  final mockService = MockFooderlichService();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO 1: Add TodayRecipeListView FutureBuilder
+    return const Center(
+      child: Text('Explore Screen'),
+    );
+  }
+}
