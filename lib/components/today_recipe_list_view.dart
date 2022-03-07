9cec3468602ded395e80b896a50a9d6e54513051
Steve Blythe <blythesr@gmail.com>
2022-03-07 20:05:51 +0000

    Implemented Horiz Scroll View in Recipe Explorer

--- lib/components/today_recipe_list_view.dart
+++ lib/components/today_recipe_list_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import '../models/explore_recipe.dart';
+import 'components.dart';
 
 class TodayRecipeListView extends StatelessWidget {
   const TodayRecipeListView({
@@ -26,13 +26,33 @@ class TodayRecipeListView extends StatelessWidget {
           const SizedBox(height: 16),
           Container(
             height: 400,
-            // TODO: Add ListView Here
-            color: Colors.grey,
+            color: Colors.transparent,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: recipes.length,
+              itemBuilder: (context, index) {
+                final recipe = recipes[index];
+                return buildCard(recipe);
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(width: 16);
+              },
+            ),
           ),
         ],
       ),
     );
   }
 
-  // TODO: Add buildCard() widget here
+  Widget buildCard(ExploreRecipe recipe) {
+    if (recipe.cardType == RecipeCardType.card1) {
+      return Card1(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card2) {
+      return Card2(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card3) {
+      return Card3(recipe: recipe);
+    } else {
+      throw Exception('This card doesn\'t exist yet');
+    }
+  }
 }
